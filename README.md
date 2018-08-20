f9520246c422dc8a893d7ff10828eabb9b64274a
Nisanthan Nanthakumar <nnutoronto2013@gmail.com>
2018-08-20 13:38:59 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Golang News Aggregator
+
+
+## Installation / Getting Started
+
+```sh
+git clone https://github.com/NisanthanNanthakumar/goNewsAgg.git
+
+cd goNewsAgg
+
+go run main.go
+```
+
+This starts up a local server on port 8000. Navigate to [http://localhost:8000](http://localhost:8000).
---
+++ main.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"encoding/xml"
+	"html/template"
+	"io/ioutil"
+	"net/http"
+	"sync"
+)
+
+var wg sync.WaitGroup
+
+type SitemapIndex struct {
+	Locations []string `xml:"sitemap>loc"`
+}
+
+type News struct {
+	Titles    []string `xml:"url>news>title"`
+	Keywords  []string `xml:"url>news>keywords"`
+	Locations []string `xml:"url>loc"`
+}
+
+type NewsMap struct {
+	Keyword  string
+	Location string
+}
+
+type NewsAggPage struct {
+	Title string
+	News  map[string]NewsMap
+}
+
+func newsRoutine(c chan News, Location string) {
+	defer wg.Done()
+	var n News
+	resp, _ := http.Get(Location)
+	bytes, _ := ioutil.ReadAll(resp.Body)
+	xml.Unmarshal(bytes, &n)
+	resp.Body.Close()
+
+	c <- n
+}
+
+func newsAggHandler(w http.ResponseWriter, r *http.Request) {
+
+	var s SitemapIndex
+	newsMap := make(map[string]NewsMap)
+	resp, _ := http.Get("https://www.washingtonpost.com/news-sitemap-index.xml")
+	bytes, _ := ioutil.ReadAll(resp.Body)
+	xml.Unmarshal(bytes, &s)
+
+	resp.Body.Close()
+	queue := make(chan News, 30)
+	for _, Location := range s.Locations {
+		wg.Add(1)
+		go newsRoutine(queue, Location)
+	}
+	wg.Wait()
+	close(queue)
+
+	for elem := range queue {
+		for idx := range elem.Titles {
+			newsMap[elem.Titles[idx]] = NewsMap{elem.Keywords[idx], elem.Locations[idx]}
+		}
+	}
+
+	p := NewsAggPage{Title: "Washington Post News Aggregator", News: newsMap}
+	t, _ := template.ParseFiles("basictemplating.html")
+	t.Execute(w, p)
+}
+
+func main() {
+	http.HandleFunc("/", newsAggHandler)
+	http.ListenAndServe(":8000", nil)
+}
